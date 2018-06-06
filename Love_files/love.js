3f0099824f378d552f1f30d783f1e48bc499d761
AFCC <39616185+weiliang12306@users.noreply.github.com>
2018-06-06 20:32:30 +0800

    Update love.js

--- Love_files/love.js
+++ Love_files/love.js
@@ -163,13 +163,13 @@
             ctx.scale(scale, scale);
             ctx.moveTo(0, 0);
     	    ctx.lineTo(15, 15);
-    	    ctx.lineTo(60, 15);
+    	    ctx.lineTo(75, 15);
             ctx.stroke();
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("click here", 23, 16);
+            ctx.font = "11px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.fillText("happy birthday！", 20, 16);
             ctx.restore();
         },
         clear: function() {
